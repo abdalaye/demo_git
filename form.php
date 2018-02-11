84c50accc6472f04c2ecac563a8f8a772512a0a0
abdalaye <abdalayesoum237@gmail.com>
2018-02-11 17:55:48 +0000

    Fichier form vient d'etre rempli

--- form.php
+++ form.php
@@ -1,1 +1,19 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulaire</title>
+</head>
+<body>
+	<form method="POST" action="resultat.php">
+		<p>
+			<label for="pseudo">Votre email : </label>
+			<input type="text" name="pseudo" id="pseudo">
+		</p>
+		<p>
+			<label for="email">Votre email : </label>
+			<input type="email" name="email" id="email">
+		</p>
+		<input type="submit" name="submit" value="envoyer">
+	</form>
+</body>
+</html>
