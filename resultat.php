c2058cdf8a0105efc11987e9927449d1fdc87238
abdalaye <abdalayesoum237@gmail.com>
2018-02-11 18:02:10 +0000

    Fichier resultat.php vient d'etre rempli

--- resultat.php
+++ resultat.php
@@ -1,1 +1,4 @@
-
+<?php
+	if (isset($_POST['submit'])) {
+		echo 'Vous venez de nous envoyer les informations suivantes : <br> Le pseudo : '.$_POST['pseudo']. '<br> L\'email : ' .$_POST['email'];
+	}
